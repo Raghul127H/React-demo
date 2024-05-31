ce9361fd9d48685182a0e50bc11fd79834f5a0ae
Raghul127H <vraghul127@gmail.com>
2024-05-31 15:14:18 +0530

    .

--- src/component/product.jsx
+++ src/component/product.jsx
@@ -1,15 +1,15 @@
-import React from 'react';
-import  { useContext } from 'react'
-import CounterContext from '../Context/CounterContext';
+// import React from 'react';
+// import  { useContext } from 'react'
+// import CounterContext from '../Context/CounterContext';
 
- function Product() {
-   const{increment}= useContext(CounterContext);
+//  function Product() {
+//    const{increment}= useContext(CounterContext);
 
-    return (
-      <div>
-       <button onClick={increment}>Add to cart</button>
-      </div>
-    )
-  }
+//     return (
+//       <div>
+//        <button onClick={increment}>Add to cart</button>
+//       </div>
+//     )
+//   }
 
-export default Product;
+// export default Product;
